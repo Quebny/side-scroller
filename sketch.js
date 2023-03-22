31fc85b831f813ba46fc7e7b37116ce1ab1191cf
Quebny <quebynliga@gmail.com>
2023-03-21 20:13:21 -0600

    moving background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
+let bgImg;
+let bg1;
+let bg2;
 
+function preload(){
+    bgImg = loadImage('assets/bg.jpg');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    bg1 = new Bg(bgImg, 0);
+    bg2 = new Bg(bgImg, width);
 }
 
 function draw(){
-    
+    bg1.draw();
+    bg2.draw();
+
+    bg1.scroll();
+    bg2.scroll();
 }
